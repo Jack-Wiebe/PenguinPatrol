b86eb65b3ecfb21735ddedd594a4e6d226f941b9
Austin Eathorne <austineathorne@gmail.com>
2017-03-20 19:59:18 -0500

    Changes to the secuirty room -Buttons can be clicked -Lever can be pulled

--- PenguinPatrol/Assets/Assets/Scripts/ButtonMove.cs
+++ PenguinPatrol/Assets/Assets/Scripts/ButtonMove.cs
@@ -5,14 +5,55 @@ using UnityEngine;
 public class ButtonMove : MonoBehaviour {
 
 	[SerializeField]
+	private Transform push;
+
+	[SerializeField]
 	private float speed = 5.0f;
 
 	[SerializeField]
 	private float range = 0.15f;
 
+	[SerializeField]
+	private float resetTime = 1.0f;
+
+	private float posY;
+
+	private bool isMoving = false;
+
 	void Update () 
 	{
-		this.transform.localPosition = new Vector3(this.transform.localPosition.x, Mathf.Sin(Time.timeSinceLevelLoad * speed) * range, this.transform.localPosition.z);
+		//this.transform.localPosition = new Vector3(this.transform.localPosition.x, Mathf.Sin(Time.timeSinceLevelLoad * speed) * range, this.transform.localPosition.z);
+	}
+
+	public void StartMove()
+	{
+		if(!isMoving)
+			StartCoroutine(MoveButton());
+	}
+
+	private IEnumerator MoveButton()
+	{
+		isMoving = true;
+		posY = push.localPosition.y;
+
+		while(posY > range)
+		{
+			posY = Mathf.MoveTowards(posY, range, Time.deltaTime * (speed/2));
+			push.localPosition = new Vector3(0.0f, posY, 0.0f);
+			yield return null;
+		}
+
+		yield return new WaitForSeconds(resetTime);
+
+		while(posY < 0.0f)
+		{
+			posY = Mathf.MoveTowards(posY, 0.0f, Time.deltaTime * speed);
+			push.localPosition = new Vector3(0.0f, posY, 0.0f);
+			yield return null;
+		}
+
+		isMoving = false;
+		yield return null;
 	}
 }
 
